0ee575316c39c3c551111a66746a8b2e1897560f
Menuka Gusain <menukagusain@gmail.com>
2020-11-29 18:04:05 -0500

    updated

---
+++ Develop/script.js
@@ -0,0 +1,58 @@
+//Assignment Code
+var generateBtn = document.querySelector(#generate");
+console.log ("generate")
+
+//custome code from here
+//Generate password function
+
+
+
+var capitalLetter = "ACDEFGHIJKLMNOPARSTUVWXYZ".split("");
+var lowerLetter = "abcdefghijklmnopqurstuvwxyz".split("");
+var specialCharacter = "!@#$%^&*()" .split ("");
+var numbers = "0123456789".split("");
+var customerPassowrd = "";
+var userInput=[];
+
+console.log (capitalLetter);
+console.log(specialCharacter);
+
+var passwordLength = prompt ("Choose your password between 6 to 128");
+
+console.log (passwordLength);
+
+if (passwordLength < 6 || passwordLength >128) {
+alert ("Choose between 8 to 128 ");
+
+}
+
+/*8var passwordCapitalletter = confirm ("Does your password contain capital letter");
+
+if (passwordCapitalletter = true) {);
+
+var passwordLowerlLetter = confirm ("Does your password contain lower letter");
+
+if (passwordLowerlLetter = true) {function upperCaseRandom() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+}
+
+var passwordNumbers = confirm ("Does your password contain Numbers");
+
+if (passwordNumbers = true) {function upperCaseRandom() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+}
+var passwordSpecialCharacter = confirm ("Does your password contain Special Characters");
+
+if (passwordSpecialCharacter = true) {function upperCaseRandom() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65); */
+}
+
+}
+ 
+
+
+
